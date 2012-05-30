d7232881b828bb42d66f16cb06954f4003205c9a
Mike Crittenden <mikecrittenden@gmail.com>
2012-05-30 14:01:05 -0400

    theming tweaks

--- sites/all/themes/projectwebapp/templates/html.tpl.php
+++ sites/all/themes/projectwebapp/templates/html.tpl.php
@@ -8,6 +8,16 @@
     <?php print $scripts; ?>
     <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,700,200,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Merriweather:400,700,300' rel='stylesheet' type='text/css'>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-6322775-18']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
   </head>
   <body class="<?php print $classes; ?>" <?php print $attributes;?>>
     <?php print $page_top; ?>
